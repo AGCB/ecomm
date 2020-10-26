dbec0a274ff29642eb3432b65a5612109eebaa89
alexClifton <oooalexoooo@gmail.com>
2020-10-25 17:52:02 -0700

    instead of a link there is also grabbing the react-router props and using props.history.push()

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './homepage.styles.scss';
 import { Link } from 'react-router-dom';
 
-const HomePage = () => (
+const HomePage = (props) => (
   <div className="homepage">
-    <Link to="/">INSERT HOME IMAGE HERE</Link>
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
           <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
+          <span ></span>
+          <button
+            className="subtitle"
+            onClick={() => props.history.push('/hats')}>click here for hats page</button>
         </div>
       </div>
       <div className="menu-item">
