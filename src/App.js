bd851d5eb3360499ee9062d39fcfa01fae0d4a90
alexClifton <oooalexoooo@gmail.com>
2020-10-25 17:35:50 -0700

    create hats route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.scss';
 import HomePage from './homepage.component.jsx';
+import { Route, Switch } from 'react-router-dom';
 
+const HatsPage = (props) => {
+  console.log(props.match.params.foo);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route path='/hats/:foo' component={HatsPage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
