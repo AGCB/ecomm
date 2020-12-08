04b440e2fb3e67686bf0aa588ccf02c2c36b18a7
alexClifton <oooalexoooo@gmail.com>
2020-12-07 18:32:57 -0800

    create homepage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.scss';
+import HomePage from './homepage.component.jsx';
+
 
 function App() {
   return (
     <div className="App">
       <span>hw</span>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      {42}
+    </div>
+  )
+}
+
+export default HomePage;
