a0b7ec304d2348c08dffc583d9d9cb04d04d8c76
alexClifton <oooalexoooo@gmail.com>
2020-12-08 21:56:23 -0800

    create init react-router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.scss';
 import HomePage from './pages/HomePage';
+import { Route, Switch } from 'react-router-dom';
 
+function HatsPage() {
+  return (
+    <span>hatsPage</span>
+  )
+}
 function App() {
   return (
     <div className="App">
       <h1>Ecomm Site</h1>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/hats" component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
