e20ba811c0f5b2d8b7d881f20369444b42cdf47d
alexClifton <oooalexoooo@gmail.com>
2020-12-13 19:25:19 -0800

    create header component

---
+++ README.md
@@ -0,0 +1,2 @@
+# ecomm
+[Andrei Neagoie inspiration](https://www.udemy.com/course/complete-react-developer-zero-to-mastery/)
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
+import Header from './components/Header';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Ecomm Site</h1>
+      <Header/>
       <Switch>
         <Route path="/shop" component={ShopPage}/>
         <Route exact path="/" component={HomePage}/>
